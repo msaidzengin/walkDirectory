7ba203472171bddac066d93bf70c3465dee09463
msaidzengin <m.zengin@etu.edu.tr>
2020-04-26 13:59:18 +0300

    names

--- walk.py
+++ walk.py
@@ -1,5 +1,7 @@
 import os
 from glob import glob
+from collections import Counter
+
 path = os.getcwd()
 result = [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*'))]
 
@@ -8,3 +10,14 @@ for r in result:
     print(r)
     f.write(r + "\n")
 f.close()
+
+
+names = []
+for r in result:
+    names.append(r.split('\\')[-1])
+print("-------")
+for i in names:
+    print(i)
+
+print("-----")
+print(Counter(names))
